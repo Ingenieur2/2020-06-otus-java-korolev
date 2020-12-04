30c3c69374b0c9f4b30677cc2d6c9ac905c79593
Ingenieur2 <pavel10589@yandex.ru>
2020-12-04 17:00:37 +0300

    hw14-SpringMVC. Done

--- hw14-SpringMVC/src/main/java/ru/package01/controller/UserController.java
+++ hw14-SpringMVC/src/main/java/ru/package01/controller/UserController.java
@@ -3,7 +3,6 @@ package ru.package01.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 import ru.package01.core.model.User;
 import ru.package01.core.service.DbServiceUser;
 
@@ -27,10 +26,5 @@ public class UserController {
         List<User> users = dbServiceUser.getAll();
         model.addAttribute("users", users);
         return "users.html";
-    }
-
-    @PostMapping({"/users"})
-    public RedirectView userSave(@ModelAttribute User user) {
-        return new RedirectView("/", true);
     }
 }
