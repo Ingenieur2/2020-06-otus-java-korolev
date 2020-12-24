738a0b9cfb301a59f081076a1d3ec981f0d0b882
Ingenieur2 <pavel10589@yandex.ru>
2020-12-24 08:26:31 +0300

    hw16-MessageSystem. Done

--- hw16-MessageSystem/WebSocket/src/main/java/ru/package01/controller/UserController.java
+++ hw16-MessageSystem/WebSocket/src/main/java/ru/package01/controller/UserController.java
@@ -2,17 +2,17 @@ package ru.package01.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import ru.package01.core.model.User;
 import ru.package01.services.FrontendService;
 
-@RestController
-public class UserRestController {
+@Controller
+public class UserController {
     private final FrontendService frontendService;
     private final SimpMessagingTemplate messagingTemplate;
 
 
-    public UserRestController(FrontendService frontendService, SimpMessagingTemplate messagingTemplate) {
+    public UserController(FrontendService frontendService, SimpMessagingTemplate messagingTemplate) {
         this.frontendService = frontendService;
         this.messagingTemplate = messagingTemplate;
     }
